71f32f169f4bcce90eaf655323c6d28a1a56b241
Pratham shetty <prathamshetty484@gmail.com>
2024-06-15 21:47:30 +0530

    added login and getotp

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,29 +2,32 @@ import twilio from 'twilio'
 import { config } from '../config/dbconfig.js';
 import sql from 'mssql'
 import JWT from "jsonwebtoken"
+import AppError from "../utils/error.util.js";
+
 const cookieoptions={
     httpOnly: true,
     secure:false,
     sameSite: "strict",
     expires: new Date(Date.now() + 7200000),
   }
-function generateJWTToken(phone,role){
+function generateJWTToken(phone,role,name){
     return JWT.sign({
+        name:name,
         ph_no:phone,
         user_role:role,
     },
-    process.env.JWT_SECRET,//secret key
+    process.env.JWT_SECRET,//secret ke
     {expiresIn:process.env.JWT_EXPIRY}//for security dont keep permanent tokens
     )
 }
 const login=async(req,res,next)=>{
     try{
-        const {ph_no,otp}=req.body;
+        const {ph_no,otp,name}=req.body;
         const pool=await sql.connect(config);
         const data=pool.request().query(`select * from user_tb where ph_no='${ph_no}'`);
         data.then(async(res1)=>{
             if(res1.recordset[0].otp==otp){
-                const token=generateJWTToken(ph_no,'USER');
+                const token=generateJWTToken(ph_no,res1.recordset[0].role,name);
                 res.cookie('token',token,cookieoptions);
 
                 const pool1=await sql.connect(config);
@@ -62,18 +65,21 @@ const login=async(req,res,next)=>{
 };
 const otp=async(req,res,next)=>{
     try{
-        const {ph_no}=req.body;
+        const {ph_no,name}=req.body;
+        let role;
+        if(ph_no==process.env.PHNO_ADMIN) role="ADMIN";
+        else role="USER"
         const ot=`${Math.floor(1000+Math.random()*9000)}`;
         //inserting entry into user_tb table
         const pool=await sql.connect(config);
-        const data=pool.request().query(`insert into user_tb values('${ph_no}','${ot}')`);
+        const data=pool.request().query(`insert into user_tb values('${name}','${ph_no}','${ot}','${role}')`);
         data.then(res1=>{
-            console.log('inserted entry !! ');
+            console.log('inserted entry ! ');
          }
          )
          //creating the otp message and sent to phone
         const client=new twilio(process.env.TWILIO_SID,process.env.TWILIO_AUTH_TOKEN);
-        client.messages.create({body:ot,from:'+17856997678',to:ph_no})
+        client.messages.create({body:ot,from:'+17856997678',to:`${ph_no}`})
         
         .then(message=>console.log('sent'))
         .catch(err=>console.log(err));
@@ -88,4 +94,25 @@ const otp=async(req,res,next)=>{
     }
 
 };
-export {login,otp};
+const logout=(req,res,next)=>{
+    try{
+        const cookieOptions={
+            expires:new Date(),
+            secure:false,
+            maxAge:0,
+            httpOnly:true
+        };
+        res.cookie("token",null,cookieOptions);
+        res.status(200).json({
+            success:true,
+            message:"logged out sucessfully"
+            
+        })
+
+    }
+    catch(error){
+        return next(new AppError(error,400));
+    }
+
+};
+export {login,otp,logout};
