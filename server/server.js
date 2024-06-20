29d7f2c2d603da3c13b64d57b139e0f994e841e5
Pratham shetty <prathamshetty484@gmail.com>
2024-06-20 14:57:48 +0530

    adding file uploads

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 import app from './app.js';
 import {connecttodb} from './config/dbconfig.js';
-
+import cloudinary from 'cloudinary'
 const port=process.env.PORT || 6002;
+
+cloudinary.v2.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
 
 app.listen(port,async()=>{
     await connecttodb();
